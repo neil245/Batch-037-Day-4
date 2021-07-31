c9a3bb8bf332ebd2ab23a8c8c521734437e10f3c
neil245 <indranilghoshal5@gmail.com>
2021-08-01 00:12:12 +0530

    The rules for the games are mentioned

--- src/com/snakeandladder/java/SnakeAndLadder.java
+++ src/com/snakeandladder/java/SnakeAndLadder.java
@@ -8,5 +8,16 @@ public static void main(String[] args) {
 	System.out.println("Initial position is"+init_position);
 	int dice = (int)Math.floor(Math.random()*6+1);
 	System.out.println("The dice roll gives : "+dice);
+	int move = (int)Math.floor(Math.random()*3);
+	System.out.println("The value of move is : "+move);
+	if(move == 0) {
+		System.out.println("No play");
+	}
+	else if(move == 1) {
+		System.out.println("The player moves according to the dice outcome");
+	}
+	else {
+		System.out.println("The player moves back according to the dice value");
+	}
 }
 }
