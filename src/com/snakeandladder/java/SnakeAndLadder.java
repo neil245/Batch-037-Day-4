1b01ad639054572e5656a40e4894feb4f18c04b4
neil245 <indranilghoshal5@gmail.com>
2021-08-01 00:19:29 +0530

    Game is played till player reaches 100

--- src/com/snakeandladder/java/SnakeAndLadder.java
+++ src/com/snakeandladder/java/SnakeAndLadder.java
@@ -5,19 +5,35 @@ public static void main(String[] args) {
 	int roll = 0;
 	System.out.println("Welcome to the Snakes and Ladders game");
 	int init_position = 0;
-	System.out.println("Initial position is"+init_position);
-	int dice = (int)Math.floor(Math.random()*6+1);
-	System.out.println("The dice roll gives : "+dice);
-	int move = (int)Math.floor(Math.random()*3);
-	System.out.println("The value of move is : "+move);
-	if(move == 0) {
-		System.out.println("No play");
+	//System.out.println("Initial position is"+init_position);
+	//int dice = (int)Math.floor(Math.random()*6+1);
+	//System.out.println("The dice roll gives : "+dice);
+	//int move = (int)Math.floor(Math.random()*3);
+	//System.out.println("The value of move is : "+move);
+	while(init_position<100) {
+		int dice = (int)Math.floor(Math.random()*6+1);
+		roll++;
+		int move = (int)Math.floor(Math.random()*3);
+		if(move == 0) {
+			continue;
+		}
+		else if(move == 1) {
+			init_position+=dice;
+			if(init_position>100) {
+				init_position =init_position-dice;
+			}
+			if(init_position == 100) {
+				System.out.println("Player wins !!");
+				break;
+			}
+		}
+		else if(move == 2) {
+			init_position = init_position-dice;
+			if(init_position<0) {
+				init_position = 0;
+			}
+		}
 	}
-	else if(move == 1) {
-		System.out.println("The player moves according to the dice outcome");
-	}
-	else {
-		System.out.println("The player moves back according to the dice value");
-	}
+	System.out.println("Number of dice rolls is"+roll);
 }
 }
