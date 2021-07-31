8464dc8b31b46b44948d85f6e0109cfb5bcdc582
neil245 <indranilghoshal5@gmail.com>
2021-08-01 00:08:14 +0530

    The dice is initialized

--- src/com/snakeandladder/java/SnakeAndLadder.java
+++ src/com/snakeandladder/java/SnakeAndLadder.java
@@ -5,6 +5,8 @@ public static void main(String[] args) {
 	int roll = 0;
 	System.out.println("Welcome to the Snakes and Ladders game");
 	int init_position = 0;
-	System.out.println(init_position);
+	System.out.println("Initial position is"+init_position);
+	int dice = (int)Math.floor(Math.random()*6+1);
+	System.out.println("The dice roll gives : "+dice);
 }
 }
