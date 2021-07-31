499ae60cd47d5810625bc3e96d27a45403fb341d
neil245 <indranilghoshal5@gmail.com>
2021-08-01 00:05:38 +0530

    Game initialized with initial position at 9

---
+++ src/com/snakeandladder/java/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.snakeandladder.java;
+import java.util.Random;
+public class SnakeAndLadder {
+public static void main(String[] args) {
+	int roll = 0;
+	System.out.println("Welcome to the Snakes and Ladders game");
+	int init_position = 0;
+	System.out.println(init_position);
+}
+}
